681e7e51d03efa0c81fceff398a8e85aa1836016
pranav-8660 <pranavcse2003@gmail.com>
2025-03-12 14:42:56 +0530

    configured message model

---
+++ chatapp-backend/src/main/java/com/pranav/microservices/chatapp_backend/model/Message.java
@@ -0,0 +1,72 @@
+package com.pranav.microservices.chatapp_backend.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Message {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String sender;
+    private String receiver;
+    private String content;
+    private LocalDateTime timestamp;
+
+    public Message(String sender, String receiver, String content, LocalDateTime timestamp) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public Message() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
