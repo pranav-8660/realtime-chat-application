3c3ed3895ff152d3e0655f6d7c2c566f082deac3
pranav-8660 <pranavcse2003@gmail.com>
2025-03-13 15:31:36 +0530

    deleted the entire code for architectural mismatch, went/choose new architecture

--- chatapp-backend/src/main/java/com/pranav/microservices/chatapp_backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pranav.microservices.chatapp_backend.repository;
-
-
-import com.pranav.microservices.chatapp_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
